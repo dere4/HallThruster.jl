934cbccfbc16c290204a0c13ad5bbf745c3e87a1
dere4 <93271216+dere4@users.noreply.github.com>
2022-04-04 15:17:13 -0400

    periodic bcs for weno, not done yet

--- src/heavy_species.jl
+++ src/heavy_species.jl
@@ -41,6 +41,53 @@ function update_heavy_species!(dU, U, params, t)
                 
             end
         end
+
+        
+        #for ion OVS test do periodic boundaries effectively
+        #cell 1 #################
+        F[index.ρn, 1] = WENO5_compute_fluxes(
+                F[index.ρn, end-2], F[index.ρn, end-1], F[index.ρn, 1], F[index.ρn, 2], F[index.ρn, 3])
+        #Handle ions
+        for Z in 1:ncharge
+            @views @. F[index.ρi[Z]:index.ρiui[Z], 1] = WENO5_compute_fluxes(
+                SA[F[index.ρi[Z], end-2], F[index.ρiui[Z], end-2]],
+                SA[F[index.ρi[Z], end-1], F[index.ρiui[Z], end-1]],
+                SA[F[index.ρi[Z], 1],   F[index.ρiui[Z], 1]],
+                SA[F[index.ρi[Z], 2], F[index.ρiui[Z], 2]],
+                SA[F[index.ρi[Z], 3], F[index.ρiui[Z], 3]],
+            )
+            
+        end
+        #cell 2 #################
+        F[index.ρn, 1] = WENO5_compute_fluxes(
+                F[index.ρn, end-1], F[index.ρn, 1], F[index.ρn, 2], F[index.ρn, 3], F[index.ρn, 4])
+        #Handle ions
+        for Z in 1:ncharge
+            @views @. F[index.ρi[Z]:index.ρiui[Z], 1] = WENO5_compute_fluxes(
+                SA[F[index.ρi[Z], end-1], F[index.ρiui[Z], end-1]],
+                SA[F[index.ρi[Z], 1], F[index.ρiui[Z], 1]],
+                SA[F[index.ρi[Z], 2], F[index.ρiui[Z], 2]],
+                SA[F[index.ρi[Z], 3], F[index.ρiui[Z], 3]],
+                SA[F[index.ρi[Z], 4], F[index.ρiui[Z], 4]],
+            )
+            
+        end
+        #cell end-2 #################
+        F[index.ρn, 1] = WENO5_compute_fluxes(
+                F[index.ρn, end-1], F[index.ρn, 1], F[index.ρn, 2], F[index.ρn, 3], F[index.ρn, 4])
+        #Handle ions
+        for Z in 1:ncharge
+            @views @. F[index.ρi[Z]:index.ρiui[Z], 1] = WENO5_compute_fluxes(
+                SA[F[index.ρi[Z], end-1], F[index.ρiui[Z], end-1]],
+                SA[F[index.ρi[Z], 1], F[index.ρiui[Z], 1]],
+                SA[F[index.ρi[Z], 2], F[index.ρiui[Z], 2]],
+                SA[F[index.ρi[Z], 3], F[index.ρiui[Z], 3]],
+                SA[F[index.ρi[Z], 4], F[index.ρiui[Z], 4]],
+            )
+            
+        end
+
+
     end
 
     @inbounds for i in 2:ncells-1
